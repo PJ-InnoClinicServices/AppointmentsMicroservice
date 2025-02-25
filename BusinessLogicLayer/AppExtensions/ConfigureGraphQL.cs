f530c09e7070bd0f61db462af3773d42847f779a
piotrjass <131187708+piotrjass@users.noreply.github.com>
2025-02-25 16:32:10 +0100

    Added services, config files

---
+++ BusinessLogicLayer/AppExtensions/ConfigureGraphQL.cs
@@ -0,0 +1,40 @@
+﻿using BusinessLogicLayer.Schema.Mutations;
+using BusinessLogicLayer.Schema.Queries;
+using DataAccessLayer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BusinessLogicLayer.AppExtensions
+{
+    public class ConfigureGraphQl
+    {
+        private readonly IConfiguration _configuration;
+
+        public ConfigureGraphQl(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public void Configure(IServiceCollection services)
+        { 
+            services.AddGraphQLServer()
+                .AddQueryType<Query>()
+                .AddMutationType<Mutation>()
+                .AddDiagnosticEventListener<ErrorLoggingDiagnosticsEventListener>();
+            
+            services.AddPooledDbContextFactory<ApplicationDbContext>(options => 
+                options.UseNpgsql(_configuration.GetConnectionString("DefaultConnection")));
+        }
+        
+        public static void ApplyMigrations(IServiceProvider serviceProvider)
+        {
+            using var scope = serviceProvider.CreateScope();
+
+            var dbContextFactory = scope.ServiceProvider.GetRequiredService<IDbContextFactory<ApplicationDbContext>>();
+            var dbContext = dbContextFactory.CreateDbContext(); 
+
+            dbContext.Database.Migrate(); 
+        }
+    }
+}
---
+++ BusinessLogicLayer/AppExtensions/ErrorLoggingDiagnosticsEventListener.cs
@@ -0,0 +1,46 @@
+﻿using HotChocolate.Execution;
+using HotChocolate.Execution.Instrumentation;
+using HotChocolate.Execution.Processing;
+using HotChocolate.Resolvers;
+using Microsoft.Extensions.Logging;
+
+namespace BusinessLogicLayer.AppExtensions;
+
+public class ErrorLoggingDiagnosticsEventListener(ILogger<ErrorLoggingDiagnosticsEventListener> log)
+    : ExecutionDiagnosticEventListener
+{
+    public override void ResolverError(
+        IMiddlewareContext context,
+        IError error)
+    {
+        log.LogError(error.Exception, error.Message);
+    }
+
+    public override void TaskError(
+        IExecutionTask task,
+        IError error)
+    {
+        log.LogError(error.Exception, error.Message);
+    }
+
+    public override void RequestError(
+        IRequestContext context,
+        Exception exception)
+    {
+        log.LogError(exception, "RequestError");
+    }
+
+    public override void SubscriptionEventError(
+        SubscriptionEventContext context,
+        Exception exception)
+    {
+        log.LogError(exception, "SubscriptionEventError");
+    }
+
+    public override void SubscriptionTransportError(
+        ISubscription subscription,
+        Exception exception)
+    {
+        log.LogError(exception, "SubscriptionTransportError");
+    }
+}
