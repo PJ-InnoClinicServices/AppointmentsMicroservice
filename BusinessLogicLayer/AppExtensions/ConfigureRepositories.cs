e528eadf09ac12a6401655dd9541b8793e061420
piotrjass <131187708+piotrjass@users.noreply.github.com>
2025-02-27 11:23:35 +0100

    Added services, config files (#6)

---
+++ BusinessLogicLayer/AppExtensions/ConfigureRepositories.cs
@@ -0,0 +1,15 @@
+﻿using DataAccessLayer;
+using DataAccessLayer.Interfaces.IRepositories;
+using DataAccessLayer.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BusinessLogicLayer.AppExtensions
+{
+    public static class ConfigureRepositories
+    {
+        public static void AddRepositories(this IServiceCollection services)
+        {
+            services.AddScoped<IAppointmentRepository, AppointmentRepository>();
+        }
+    }
+}
---
+++ BusinessLogicLayer/AppExtensions/ConfigureServices.cs
@@ -0,0 +1,13 @@
+﻿using BusinessLogicLayer.Interfaces.IServices;
+using BusinessLogicLayer.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BusinessLogicLayer.AppExtensions;
+
+public static class ConfigureServices
+{
+    public static void AddServices(this IServiceCollection services)
+    {
+        services.AddScoped<IAppointmentService, AppointmentService>();
+    }
+}
